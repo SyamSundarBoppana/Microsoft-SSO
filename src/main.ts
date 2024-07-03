f649cd250e643d00ac22f0461897e0990046582f
SyamSundarBoppana <boppana.syamsundar@gmail.com>
2024-07-03 22:38:27 +0530

    Removed "api" to empty string in swagger module

--- src/main.ts
+++ src/main.ts
@@ -18,7 +18,8 @@ async function bootstrap() {
     )
     .build();
   const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
+  SwaggerModule.setup('', app, document);
   await app.listen(4000);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
